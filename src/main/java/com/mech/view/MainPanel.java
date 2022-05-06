06cc7948a1838276e85a9ea60377b02add3c7fde
Damian1142 <83139509+Damian1142@users.noreply.github.com>
2022-05-06 22:14:53 +0200

    1.2

--- src/main/java/com/mech/view/MainPanel.java
+++ src/main/java/com/mech/view/MainPanel.java
@@ -2,18 +2,24 @@ package com.mech.view;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.ArrayList;
-
+import javax.sound.sampled.*;
 import javax.swing.*;
-
 import com.mech.Main;
 import com.mech.input.InputKey;
 import com.mech.snake.Snake;
 import com.mech.view.przeszkody.Obstacle;
 import com.mech.view.przeszkody.SpawnType;
 import com.mech.view.przeszkody.Spawner;
+import lombok.SneakyThrows;
 
 public class MainPanel extends JPanel {
+    @SneakyThrows
     public MainPanel() {
         drawings = new ArrayList<>();
         setPreferredSize(new Dimension(Board.MAX_X,Board.MAX_Y));
@@ -32,12 +38,28 @@ public class MainPanel extends JPanel {
             if (snake.eat()){
                 Spawner.spawn(SpawnType.FOOD);
                 snake.addBody();
+                pick();
             }
             ViewController.score.setText("Wynik: " + (snake.getBody().size() - 3));
             repaint();
         });
         timer.start();
         addAllComponent();
+        new Thread(() -> {
+            try {
+                // Open an audio input stream.
+                URL url = this.getClass().getClassLoader().getResource("songs/YourLove.wav");
+                assert url != null;
+                AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+                // Get a sound clip resource.
+                Clip clip = AudioSystem.getClip();
+                // Open audio clip and load samples from the audio input stream.
+                clip.open(audioIn);
+                clip.loop(10000);
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
+        }).start();
         //staticSnake = snake;
     }
     //public static Snake staticSnake;
@@ -65,5 +87,16 @@ public class MainPanel extends JPanel {
 
     public void addAllComponent(){
         elementList.add(snake);
+    }
+    @SneakyThrows
+    public void pick(){
+        URL pickURL = this.getClass().getClassLoader().getResource("songs/pick.wav");
+        AudioInputStream pickIn;
+        Clip clip;
+        assert pickURL != null;
+        pickIn = AudioSystem.getAudioInputStream(pickURL);
+        clip = AudioSystem.getClip();
+        clip.open(pickIn);
+        clip.start();
     }
 }
